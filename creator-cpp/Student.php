daf228decd2f1d3d696c0f636bd20618ce7b57e3
Abdulsamia Alashiq <alashiq@Abdulsamias-MacBook-Pro.local>
2023-01-19 18:48:45 +0200

    upload cpp-genrator for crud dashboard

---
+++ creator-cpp/Student.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Student extends Model
+{
+    use HasFactory;
+    public $timestamps = false;
+
+    protected $fillable = [
+        'first_name',
+'middle_name',
+'last_name',
+'age',
+'gender',
+
+        'state',
+    ];
+
+}
