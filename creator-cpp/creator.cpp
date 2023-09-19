9c16af9482ff3e7284607560b43b7b2730082465
Abdulsamia Alashiq <alashiq@Abdulsamias-MacBook-Pro.local>
2023-09-19 16:54:44 +0200

    change in creator cpp to read from txt file

--- creator-cpp/creator.cpp
+++ creator-cpp/creator.cpp
@@ -5,7 +5,7 @@
 
 using namespace std;
 
-int main()
+int maina()
 {
 
     Column column[20];
@@ -57,7 +57,6 @@ int main()
         else if (column[i].type == "6")
             column[i].type = "json";
 
-
         cout << "Enter IF Search Y/N = ";
         cin >> column[i].search;
 
@@ -65,16 +64,72 @@ int main()
         cin >> column[i].showInList;
     }
 
-    // create_controller(element, column);
-    // create_model(element, column);
-    // create_table(element, column);
-    // create_permissions(element, column);
-    // create_route_api(element, column);
+    create_controller(element, column);
+    create_model(element, column);
+    create_table(element, column);
+    create_permissions(element, column);
+    create_route_api(element, column);
     create_js_lists(element, column);
     create_js_item(element, column);
     create_js_edit_item(element, column);
     create_js_new_item(element, column);
-    // create_js_route_dataservices(element, column);
+    create_js_route_dataservices(element, column);
+
+    return 0;
+}
+
+int main()
+{
+    Column column[20];
+    Element element;
+
+    string filename;
+    cout << "Enter the name of the text file: ";
+    cin >> filename;
+
+    ifstream inputFile(filename);
+    if (!inputFile.is_open())
+    {
+        cout << "Failed to open the file." << endl;
+        return 1;
+    }
+
+    // Read element properties from the file
+    inputFile >> element.model;
+    inputFile >> element.models;
+    inputFile >> element.table;
+    inputFile >> element.single;
+    inputFile >> element.arabicSingle;
+    inputFile >> element.arabic;
+    inputFile >> element.columnCount;
+    inputFile >> element.pageId;
+
+    // Read column properties from the file
+    for (int i = 0; i < element.columnCount; i++)
+    {
+        inputFile >> column[i].name;
+        inputFile >> column[i].arabic;
+        inputFile >> column[i].type;
+
+        inputFile >> column[i].search;
+        inputFile >> column[i].showInList;
+
+
+    }
+
+    inputFile.close();
+
+    // Rest of the code...
+    create_controller(element, column);
+    create_model(element, column);
+    create_table(element, column);
+    create_permissions(element, column);
+    create_route_api(element, column);
+    create_js_lists(element, column);
+    create_js_item(element, column);
+    create_js_edit_item(element, column);
+    create_js_new_item(element, column);
+    create_js_route_dataservices(element, column);
 
     return 0;
 }
